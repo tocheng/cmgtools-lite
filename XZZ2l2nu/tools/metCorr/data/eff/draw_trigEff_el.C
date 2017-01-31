e26fa361cc8b0b807a228fd7108dcabc3881108a
Hengne <Hengne.Li@CERN.CH>
2017-01-31 15:48:50 +0100

    add scripts draw muon trigger efficiency

--- XZZ2l2nu/tools/metCorr/data/eff/draw_trigEff_el.C
+++ XZZ2l2nu/tools/metCorr/data/eff/draw_trigEff_el.C
@@ -1,43 +1,88 @@
 {
 
   gROOT->ProcessLine(".x tdrstyle.C");
-  TFile* file1 = TFile::Open("eff_out_ell1pteta_36p46recalib.root");
-  //TFile* file1 = TFile::Open("trigereff12p9_20160902.root");
-  TH2D* h2d1 = (TH2D*)file1->Get("ell1pteta");
-  h2d1->GetXaxis()->SetTitle("p_{T} (GeV)");
-  h2d1->GetYaxis()->SetTitle("|#eta|");
-  h2d1->GetXaxis()->SetRangeUser(100,1000);
+  TFile* file1 = TFile::Open("hlt115electron_2016fulleff_absetapt.root");
+  TH2D* h2d1 = (TH2D*)file1->Get("efficiency_dt");
+  TH2D* h2d2 = (TH2D*)file1->Get("efficiency_mc");
+  TH2D* h2d3 = (TH2D*)file1->Get("scalefactor");
+  h2d1->GetYaxis()->SetTitle("p_{T} (GeV)");
+  h2d1->GetXaxis()->SetTitle("|#eta|");
   h2d1->SetTitleSize(0.06,"XYZ");
-  
-  std::string  lumiTag = "CMS 13 TeV 2016 L=36.46 fb^{-1}";
-  //std::string  lumiTag = "CMS 13 TeV 2016 L=12.9 fb^{-1}";
-  //if (doMC) lumiTag = "CMS 13 TeV Simulation for 2016 Data";
+  h2d2->GetYaxis()->SetTitle("p_{T} (GeV)");
+  h2d2->GetXaxis()->SetTitle("|#eta|");
+  h2d2->SetTitleSize(0.06,"XYZ");
+  h2d3->GetYaxis()->SetTitle("p_{T} (GeV)");
+  h2d3->GetXaxis()->SetTitle("|#eta|");
+  h2d3->SetTitleSize(0.06,"XYZ");
 
-  TPaveText* lumipt = new TPaveText(0.2,0.9,0.8,0.98,"brNDC");
-  lumipt->SetBorderSize(0);
-  lumipt->SetTextAlign(12);
-  lumipt->SetFillStyle(0);
-  lumipt->SetTextFont(42);
-  lumipt->SetTextSize(0.03);
-  lumipt->AddText(0.15,0.3, lumiTag.c_str()); 
+
+  
+  std::string  lumiTag_dt = "CMS 13 TeV 2016 L=36.814 fb^{-1}";
+  std::string  lumiTag_mc = "CMS 13 TeV Simulation for 2016 Data";
+
+  TPaveText* lumipt_dt = new TPaveText(0.2,0.9,0.8,0.98,"brNDC");
+  lumipt_dt->SetBorderSize(0);
+  lumipt_dt->SetTextAlign(12);
+  lumipt_dt->SetFillStyle(0);
+  lumipt_dt->SetTextFont(42);
+  lumipt_dt->SetTextSize(0.03);
+  lumipt_dt->AddText(0.15,0.3, lumiTag_dt.c_str()); 
+
+  TPaveText* lumipt_mc = new TPaveText(0.2,0.9,0.8,0.98,"brNDC");
+  lumipt_mc->SetBorderSize(0);
+  lumipt_mc->SetTextAlign(12);
+  lumipt_mc->SetFillStyle(0);
+  lumipt_mc->SetTextFont(42);
+  lumipt_mc->SetTextSize(0.03);
+  lumipt_mc->AddText(0.15,0.3, lumiTag_mc.c_str());
 
  
   TCanvas* plots = new TCanvas("plots", "plots");
-  //plots->SetLogx();
-  h2d1->GetXaxis()->SetRangeUser(120,1000);
-  h2d1->Draw("colz text45");
-  lumipt->Draw();
-  plots->SaveAs("trigeff36p46_el_pt120to1k.pdf");
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf[");
+
+
 
   plots->Clear();
-  h2d1->GetXaxis()->SetRangeUser(90,120);
+  h2d1->GetYaxis()->SetRangeUser(90,150);
   h2d1->Draw("colz text45");
-  lumipt->Draw();
-  plots->SaveAs("trigeff36p46_el_pt90to120.pdf");
+  lumipt_dt->Draw();
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf");
+
+  plots->Clear();
+  h2d1->GetYaxis()->SetRangeUser(150,1000);
+  h2d1->Draw("colz text45");
+  lumipt_dt->Draw();
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf");
+
+
+  plots->Clear();
+  h2d2->GetYaxis()->SetRangeUser(90,150);
+  h2d2->Draw("colz text45");
+  lumipt_mc->Draw();
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf");
+
+  plots->Clear();
+  h2d2->GetYaxis()->SetRangeUser(150,1000);
+  h2d2->Draw("colz text45");
+  lumipt_mc->Draw();
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf");  
 
   
 
-  
+  plots->Clear();
+  h2d3->GetYaxis()->SetRangeUser(90,150);
+  h2d3->Draw("colz text45");
+  lumipt_dt->Draw();
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf");
+
+  plots->Clear();
+  h2d3->GetYaxis()->SetRangeUser(150,1000);
+  h2d3->Draw("colz text45");
+  lumipt_dt->Draw();
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf");
+
+
+  plots->Print("hlt115electron_2016fulleff_absetapt.pdf]");
 
 
 }
--- XZZ2l2nu/tools/metCorr/macros/musf/merge_mu_trg.C
+++ XZZ2l2nu/tools/metCorr/macros/musf/merge_mu_trg.C
@@ -23,6 +23,8 @@
   TH2F* h_eff_trg_mu50tkmu50_dt_2_tmp = (TH2F*)f_mu50tkmu50_own_2->Get("efficiency_dt");
   TH2F* h_eff_trg_mu50tkmu50_mc_1_tmp = (TH2F*)f_mu50tkmu50_own_1->Get("efficiency_mc");
   TH2F* h_eff_trg_mu50tkmu50_mc_2_tmp = (TH2F*)f_mu50tkmu50_own_2->Get("efficiency_mc");
+  TH2F* h_eff_trg_mu50tkmu50_sf_1_tmp = (TH2F*)f_mu50tkmu50_own_1->Get("scalefactor");
+  TH2F* h_eff_trg_mu50tkmu50_sf_2_tmp = (TH2F*)f_mu50tkmu50_own_2->Get("scalefactor");
 
   Int_t nbinsx = h_eff_trg_mu50tkmu50_dt_1_tmp->GetYaxis()->GetNbins();
   Int_t nbinsy = h_eff_trg_mu50tkmu50_dt_1_tmp->GetXaxis()->GetNbins();
@@ -34,6 +36,9 @@
   TH2F* h_eff_trg_mu50tkmu50_dt_2 = new TH2F("h_eff_trg_mu50tkmu50_dt_2", "h_eff_trg_mu50tkmu50_dt_2", nbinsx, xbins, nbinsy, ybins);
   TH2F* h_eff_trg_mu50tkmu50_mc_1 = new TH2F("h_eff_trg_mu50tkmu50_mc_1", "h_eff_trg_mu50tkmu50_mc_1", nbinsx, xbins, nbinsy, ybins);
   TH2F* h_eff_trg_mu50tkmu50_mc_2 = new TH2F("h_eff_trg_mu50tkmu50_mc_2", "h_eff_trg_mu50tkmu50_mc_2", nbinsx, xbins, nbinsy, ybins);
+  TH2F* h_eff_trg_mu50tkmu50_sf_1 = new TH2F("h_eff_trg_mu50tkmu50_sf_1", "h_eff_trg_mu50tkmu50_sf_1", nbinsx, xbins, nbinsy, ybins);
+  TH2F* h_eff_trg_mu50tkmu50_sf_2 = new TH2F("h_eff_trg_mu50tkmu50_sf_2", "h_eff_trg_mu50tkmu50_sf_2", nbinsx, xbins, nbinsy, ybins);
+
 
   for (int ix=1; ix<=nbinsx; ix++){
     for (int iy=1; iy<=nbinsy; iy++){
@@ -41,10 +46,15 @@
       h_eff_trg_mu50tkmu50_dt_2->SetBinContent(ix, iy, h_eff_trg_mu50tkmu50_dt_2_tmp->GetBinContent(iy, ix));
       h_eff_trg_mu50tkmu50_mc_1->SetBinContent(ix, iy, h_eff_trg_mu50tkmu50_mc_1_tmp->GetBinContent(iy, ix));
       h_eff_trg_mu50tkmu50_mc_2->SetBinContent(ix, iy, h_eff_trg_mu50tkmu50_mc_2_tmp->GetBinContent(iy, ix));
+      h_eff_trg_mu50tkmu50_sf_1->SetBinContent(ix, iy, h_eff_trg_mu50tkmu50_sf_1_tmp->GetBinContent(iy, ix));
+      h_eff_trg_mu50tkmu50_sf_2->SetBinContent(ix, iy, h_eff_trg_mu50tkmu50_sf_2_tmp->GetBinContent(iy, ix));
+
       h_eff_trg_mu50tkmu50_dt_1->SetBinError(ix, iy, h_eff_trg_mu50tkmu50_dt_1_tmp->GetBinError(iy, ix));
       h_eff_trg_mu50tkmu50_dt_2->SetBinError(ix, iy, h_eff_trg_mu50tkmu50_dt_2_tmp->GetBinError(iy, ix));
       h_eff_trg_mu50tkmu50_mc_1->SetBinError(ix, iy, h_eff_trg_mu50tkmu50_mc_1_tmp->GetBinError(iy, ix));
       h_eff_trg_mu50tkmu50_mc_2->SetBinError(ix, iy, h_eff_trg_mu50tkmu50_mc_2_tmp->GetBinError(iy, ix));
+      h_eff_trg_mu50tkmu50_sf_1->SetBinError(ix, iy, h_eff_trg_mu50tkmu50_sf_1_tmp->GetBinError(iy, ix));
+      h_eff_trg_mu50tkmu50_sf_2->SetBinError(ix, iy, h_eff_trg_mu50tkmu50_sf_2_tmp->GetBinError(iy, ix));
 
     }
   }
@@ -57,6 +67,10 @@
   //TH2F* h_eff_trg_mu50tkmu50_mc_2 = (TH2F*)f_mu50tkmu50_pog_2->Get("Mu50_OR_TkMu50_PtEtaBins/efficienciesMC/abseta_pt_MC");
   TH2F* h_eff_trg_mu50tkmu50_mc_3 = (TH2F*)f_mu50tkmu50_pog_3->Get("Mu50_OR_TkMu50_PtEtaBins/efficienciesMC/abseta_pt_MC");
   TH2F* h_eff_trg_mu50tkmu50_mc_4 = (TH2F*)f_mu50tkmu50_pog_4->Get("Mu50_OR_TkMu50_PtEtaBins/efficienciesMC/abseta_pt_MC");
+
+  TH2F* h_eff_trg_mu50tkmu50_sf_3 = (TH2F*)f_mu50tkmu50_pog_3->Get("Mu50_OR_TkMu50_PtEtaBins/abseta_pt_ratio");
+  TH2F* h_eff_trg_mu50tkmu50_sf_4 = (TH2F*)f_mu50tkmu50_pog_4->Get("Mu50_OR_TkMu50_PtEtaBins/abseta_pt_ratio");
+
 
   TH2F* h_eff_trg_mu50_dt_1 = (TH2F*)f_mu50_own_1->Get("efficiency_dt");
   TH2F* h_eff_trg_mu50_dt_2 = (TH2F*)f_mu50_own_2->Get("efficiency_dt");
@@ -75,6 +89,10 @@
   h_eff_trg_mu50tkmu50_mc_2->Write("h_eff_trg_mu50tkmu50_mc_2");
   h_eff_trg_mu50tkmu50_mc_3->Write("h_eff_trg_mu50tkmu50_mc_3");
   h_eff_trg_mu50tkmu50_mc_4->Write("h_eff_trg_mu50tkmu50_mc_4");
+  h_eff_trg_mu50tkmu50_sf_1->Write("h_eff_trg_mu50tkmu50_sf_1");
+  h_eff_trg_mu50tkmu50_sf_2->Write("h_eff_trg_mu50tkmu50_sf_2");
+  h_eff_trg_mu50tkmu50_sf_3->Write("h_eff_trg_mu50tkmu50_sf_3");
+  h_eff_trg_mu50tkmu50_sf_4->Write("h_eff_trg_mu50tkmu50_sf_4");
 
   h_eff_trg_mu50_dt_1->Write("h_eff_trg_mu50_dt_1");
   h_eff_trg_mu50_dt_2->Write("h_eff_trg_mu50_dt_2");
